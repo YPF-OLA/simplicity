157f8b140fa9da92172b0f992cbbb7fd69a041ac
YPF-OLA <ygor.pfaria135@gmail.com>
2024-10-09 11:33:33 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+const botaoMenu = document.querySelector(".titulo-menu a");
+
+//selecione a lista de links do menu
+const listasDeLinks = document.querySelector(".links-menu");
+
+botaoMenu.addEventListener("click", function( event ){
+
+    //Anulando o comportamento patrão de recarregar a página atraves de um link.
+    event.preventDefault();
+    
+    listasDeLinks.classList.toggle("aberto")
+    
+});
